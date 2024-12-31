70e5fb6f40f1ff627f10e821752cefc5d36de4ea
Benjamin HAISE <43814190+HaiseB@users.noreply.github.com>
2024-12-31 15:31:51 +0100

    initial commit + gitignore created with https://www.toptal.com/developers/gitignore/api/aspnetcore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace watchlistMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
